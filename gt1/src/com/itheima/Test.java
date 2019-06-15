739b01ee20842668239dd8641f1ee7931f785611
xiaoxiang <420106233@qq.com>
2019-06-15 18:46:59 +0800

    添加git工程

---
+++ gt1/src/com/itheima/Test.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("啦啦啦啦");
+        System.out.println("啦啦啦啦");
+        System.out.println("啦啦啦啦");
+        System.out.println("啦啦啦啦");
+        System.out.println("啦啦啦啦");
+        System.out.println("啦啦啦啦");
+    }
+}
